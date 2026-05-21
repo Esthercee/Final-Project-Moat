be7b56242d1f4d3b596fc079933eb9db025d0215
Esther <esthercee006@gmail.com>
2026-05-21 12:16:03 +0100

    First commit

---
+++ vendors/vendors-data.js
@@ -0,0 +1,36 @@
+const vendors = [
+  {
+    id: "Item7",
+    name: "Item7(go)",
+    logo: "https://item7.vercel.app/assets/Item7-gupESEli.png",
+    category: "Nigerian dish",
+    rating: "4.8",
+    menu: [
+      { name: "Jollof Rice", price: "₦4,300", description: "Party-style smoky jollof" },
+      { name: "Fried Rice", price: "₦4,000", description: "With fried fish and plantain" },
+      { name: "Chicken Shawarma", price: "₦4,000", description: "Creamy and chicken filled shawarma" },
+      { name: "Beef Shawarma", price: "₦4,000", description: "Creamy and beef filled shawarma" }
+    ]
+  },
+  {
+    id: "burgerKing",
+    name: "BurgerKingLogo",
+    logo:"images/burger_king.png",
+    category: "Light Snack",
+    rating: "4.6",
+    menu: [
+      { name: "Chicken burger", price: "₦3,500", description: "Filling chicken burger" }
+    ]
+  }, 
+   {
+    id: "chicken_rep",
+    name: "Chickenreplogo",
+    logo: "images/chicken_republic.png",
+    category: "Nigerian dish",
+    rating: "4.6",
+    menu: [
+      { name: "Refuel Meal", price: "₦5,500", description: "Full pack of delicious rice combo" }
+    ]
+  }, 
+  // Just keep adding vendors here — no new pages needed!
+];
